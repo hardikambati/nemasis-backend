0b0e1b1d21c4461d1ebae666f9c4b2857b1a52cb
hardikambati <hardikambati69@gmail.com>
2021-11-30 19:40:51 +0530

    heroku changes

--- nemesis/settings.py
+++ nemesis/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
